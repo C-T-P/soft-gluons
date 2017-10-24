ddc859d4fd66e332491eb0e3ad1397d1e545c907
Christian Preuss <CPreuss1@gmx.de>
2017-10-24 17:19:57 +0200

    Correct the basis matrices C_12, C_13 and C_14

--- eikonal.py
+++ eikonal.py
@@ -6,7 +6,7 @@ import interface as sh
 def writeRunCard(E):
     pid_1 = 1
     pid_2 = -1
-    pid_3 = 1 
+    pid_3 = 1
     pid_4 = -1
     
     with open('Run.dat', 'w') as f:
@@ -32,33 +32,23 @@ def calcTrace (p_1, p_2, p_3, p_4, p_s):
     t = minkprod(np.subtract(p_1, p_3), np.subtract(p_1, p_3))
     u = minkprod(np.subtract(p_1, p_4), np.subtract(p_1, p_4))
     
-    #s = (p_1[0]+p_2[0])**2-(p_1[1]+p_2[1])**2-(p_1[2]+p_2[2])**2-(p_1[3]+p_2[3])**2
-    #t = (p_1[0]-p_3[0])**2-(p_1[1]-p_3[1])**2-(p_1[2]-p_3[2])**2-(p_1[3]-p_3[3])**2
-    #u = (p_1[0]-p_4[0])**2-(p_1[1]-p_4[1])**2-(p_1[2]-p_4[2])**2-(p_1[3]-p_4[3])**2
-    #print s, t, u
-    
     # qqbar -> qqbar using the phd thesis
-    chi_1 = 2.*pow(C_F,2)/pow(N_C,4)*(pow(t,2)+pow(u,2))/pow(s,2)
-    chi_2 = 2.*C_F/pow(N_C,3)*(pow(u,2)/(s*t)-1./N_C*chi_1)
-    chi_3 = 1./pow(N_C,2)*(2.*(pow(s,2)+pow(u,2))/pow(t,2)+2./pow(N_C,2)*(pow(t,2)+pow(u,2))/pow(s,2)-4./N_C*pow(u,2)/(s*t))
-        
-    H = pow(alpha_s, 2)*np.array([[chi_1, chi_2], [chi_2, chi_3]])
+    chi_1 = (pow(t,2)+pow(u,2))/pow(s,2)
+    chi_2 = N_C*pow(u,2)/(s*t)-chi_1
+    chi_3 = (pow(s,2)+pow(u,2))/pow(t,2)+1./pow(N_C,2)*(pow(t,2)+pow(u,2))/pow(s,2)-2./N_C*pow(u,2)/(s*t)
     
-    #H = np.array([[2*pow(C_F,2)/pow(N_C, 4)*chi_1, 2*C_F/pow(N_C, 4)*chi_2],
-                #[2*C_F/pow(N_C, 4)*chi_2, 2/pow(N_C, 2)*chi_3]])
+    H = 2./pow(N_C,2)*np.array([[pow(C_F,2)/pow(N_C,2)*chi_1, C_F/pow(N_C, 2)*chi_2],
+                [C_F/pow(N_C,2)*chi_2, chi_3]])
     M = np.array([[pow(N_C,2), 0.],
                 [0., 1./4.*(pow(N_C,2)-1)]])
         
-    Gamma = (minkprod(p_1, p_2)/(minkprod(p_1, p_s)*minkprod(p_2, p_s)) + minkprod(p_3, p_4)/(minkprod(p_3, p_s)*minkprod(p_4, p_s))) *np.array([[C_F, 1./2],[0, -1./(2.*N_C)]]) + (minkprod(p_1, p_3)/(minkprod(p_1, p_s) + minkprod(p_3, p_s)) + minkprod(p_2, p_4)/(minkprod(p_2, p_s)*minkprod(p_4, p_s))) * np.array([[C_F, 1./2],[1./2., -1./(2.*N_C)]]) + ( minkprod(p_1, p_4)/(minkprod(p_1, p_s)*minkprod(p_4, p_s)) + minkprod(p_2, p_3)/(minkprod(p_2, p_s)*minkprod(p_3, p_s)) ) * np.array([[-1./(2.*N_C), 0.],[1./2., C_F]])
-    
-    print Gamma
-    
-    T1 = np.array([[1.0, 1./N_C],[0., 2.]])
-    T2 = np.array([[1.0, -1./N_C],[0., 1./2.]])
-    Gamma = np.matmul(np.matmul(T1, Gamma), T2)
+    Gamma = (minkprod(p_1, p_2)/(minkprod(p_1, p_s)*minkprod(p_2, p_s)) + minkprod(p_3, p_4)/(minkprod(p_3, p_s)*minkprod(p_4, p_s)))*np.array([[C_F, -C_F/(2.*N_C)+1./4.*(1.-1./pow(N_C,2))],[0., -1./(2.*N_C)]]) + (minkprod(p_1, p_3)/(minkprod(p_1, p_s)*minkprod(p_3, p_s)) + minkprod(p_2, p_4)/(minkprod(p_2, p_s)*minkprod(p_4, p_s))) * np.array([[0., 1./4.*(1.-1./pow(N_C,2))],[1., -1./N_C]]) + (minkprod(p_1, p_4)/(minkprod(p_1, p_s)*minkprod(p_4, p_s)) + minkprod(p_2, p_3)/(minkprod(p_2, p_s)*minkprod(p_3, p_s)) )*np.array([[0., C_F/(2.*N_C)],[1., -1./(2.*N_C)+C_F]])
+        
+    #T1 = np.array([[1.0, 1./N_C],[0., 2.]])
+    #T2 = np.array([[1.0, -1./(2*N_C)],[0., 1./2.]])
+    #Gamma = np.matmul(np.matmul(T1, Gamma), T2)
     
     trace = np.trace(np.matmul(H,np.matmul(np.conj(Gamma.transpose()),np.matmul(M, Gamma)))) 
-    print trace
     if trace.imag < eps:
         trace = trace.real
     
@@ -72,7 +62,7 @@ phi_H = m.pi/10.
 phi_s = m.pi/7.
 R_s = np.array([])
 lambda_s = np.array([])
-maxrange = 10
+maxrange = 2
 
 writeRunCard(E)
 
@@ -88,6 +78,8 @@ for i in range (0,maxrange):
     MatEl = sh.calcMatEl(p_1, p_2, p_3, p_4, p_s)
     trace = calcTrace(p_1, p_2, p_3, p_4, p_s)
     R_s = np.append(R_s, [alpha_s/m.pi * trace/MatEl])
+    
+    print lambda_s[i], k_s
     
 print "lambda_s\tR_s"
 for i in range(0,maxrange):
