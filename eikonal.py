171ee78db1f47ed9ebc37dc48a9379db6915ec9c
Christian Preuss <CPreuss1@gmx.de>
2017-10-26 15:20:51 +0200

    Add plotting of R_s vs lambda_s

--- eikonal.py
+++ eikonal.py
@@ -1,7 +1,11 @@
 import math as m
 import cmath as cm
 import numpy as np
+import matplotlib.pyplot as plt
 import interface as sh
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('text', usetex=True)
 
 def writeRunCard(E):
     pid_1 = 1
@@ -15,7 +19,7 @@ def writeRunCard(E):
         f.write("\n\n(isr){\n\tPDF_LIBRARY None;\n}(isr)")
         f.write("\n\n(processes){\n\tProcess %d %d -> %d %d %d;\n\tME_Generator Amegic;\n\tEnd process;\n}(processes)" % (pid_1, pid_2, pid_3, pid_4, 21))
         
-    return;
+    return "$qq \\to qq(g)$";
 
 def minkprod(a, b):
     metric = [[1., 0., 0., 0.],[0.,-1., 0., 0.],[0., 0., -1., 0.], [0., 0., 0., -1.]]
@@ -26,13 +30,13 @@ def calcTrace (p_1, p_2, p_3, p_4, p_s):
     N_C = 3.
     C_F = 4./3.
     C_A = 3.
-    hard_p = np.array([p_1, p_2, p_3, p_4])
     
+    hard_p = 1.e9*np.array([p_1, p_2, p_3, p_4]) # factor of 10^9 due to GeV
+        
     s = minkprod(np.add(p_1, p_2), np.add(p_1, p_2))
     t = minkprod(np.subtract(p_1, p_3), np.subtract(p_1, p_3))
     u = minkprod(np.subtract(p_1, p_4), np.subtract(p_1, p_4))
     
-    # qqbar -> qqbar using the phd thesis
     chi_1 = (pow(t,2)+pow(u,2))/pow(s,2)
     chi_2 = N_C*pow(u,2)/(s*t)-chi_1
     chi_3 = (pow(s,2)+pow(u,2))/pow(t,2)+1./pow(N_C,2)*(pow(t,2)+pow(u,2))/pow(s,2)-2./N_C*pow(u,2)/(s*t)
@@ -77,14 +81,12 @@ phi_44 = m.pi * 5./2.
 phi_H = m.pi/10.
 phi_s = m.pi/7.
 R_s = np.array([])
-lambda_s = np.array([])
-maxrange = 9
+lambda_s = np.array([1.e-6, 5.e-6, 1.e-5, 5.e-5, 1.e-4, 5.e-4, 1.e-3, 5e-3, 1.e-2, 5.e-2, 1.e-1, 5e-1, 1.])
 
-writeRunCard(E)
+plot_title = writeRunCard(E)
 
-for i in range (0, maxrange):
-    lambda_s = np.append(lambda_s, 1.0*pow(10, -i))
-    k_s = 2.*E*lambda_s[i]
+for l_s in lambda_s:
+    k_s = 2.*E*l_s
     p_1 = [E,0.,0.,E]
     p_2 = [E,0.,0.,-E]    
     p_3 = [E,E*m.cos(phi_43+phi_H),E*m.sin(phi_43+phi_H),0]
@@ -92,16 +94,20 @@ for i in range (0, maxrange):
     p_s = [k_s,k_s*m.cos(phi_s),k_s*m.sin(phi_s),0.]
     
     MatEl = sh.calcMatEl(p_1, p_2, p_3, p_4, p_s)
-    p_1 = [x*1e9 for x in p_1]
-    p_2 = [x*1e9 for x in p_2]
-    p_3 = [x*1e9 for x in p_3]
-    p_4 = [x*1e9 for x in p_4]
-    p_s = [x*1e9 for x in p_s]
     trace = calcTrace(p_1, p_2, p_3, p_4, p_s)
     R_s = np.append(R_s, [alpha_s/m.pi* trace/MatEl])
         
 print "lambda_s\tR_s"
-for i in range(0,maxrange):
+for i in range(0, lambda_s.size):
     print lambda_s[i], "\t\t", R_s[i]
+        
+#Plotting
+plt.title(plot_title)
+plt.xlabel('$\lambda_s$')
+plt.ylabel('$R_s$')
+plt.xscale('log')
+plt.plot(lambda_s, R_s)
+plt.show()
+
     
     
