a483c7c54a071806ae2487a16018f9000a76ad22
Christian Preuss <CPreuss1@gmx.de>
2017-11-09 08:54:34 +0100

    Add automatic process identification

--- interface.py
+++ interface.py
@@ -5,31 +5,86 @@ import math as m
 sys.path.append("/home/preuss10/Compiled/lib/python2.7/site-packages/")
 import Sherpa
 
-def calcMatEl (p_1, p_2, p_3, p_4, p_s):
+def calcMatEl (process, p_1, p_2, p_3, p_4, p_s):
     # Add this to the execution arguments to prevent Sherpa from starting the cross section integration
     sys.argv.append('INIT_ONLY=2')
 
     Generator=Sherpa.Sherpa()
+    
+    if process[0] == 1:
+        if process[1] == 1:
+            pid_1 = 1
+            pid_2 = -1
+            pid_3 = 1 
+            pid_4 = -1
+            
+        elif process[1] == 2:
+            pid_1 = 1
+            pid_2 = -1
+            pid_3 = 2
+            pid_4 = -2
+            
+        elif process[1] == 3:
+            pid_1 = 2
+            pid_2 = -1
+            pid_3 =  2
+            pid_4 = -1
+            
+    elif process[0] == 2:
+        if process[1] == 1:
+            pid_1 = 1
+            pid_2 = 1
+            pid_3 = 1
+            pid_4 = 1
+            
+        elif process[1] == 2:
+            pid_1 = 1
+            pid_2 = 2
+            pid_3 = 1
+            pid_4 = 2
+        
+    elif process[0] == 3:
+        pid_1 = 1
+        pid_2 = 21
+        pid_3 = 1
+        pid_4 = 21
+            
+    elif process[0] == 4:
+        if process[1] == 1:
+            pid_1 = 1
+            pid_2 = -1
+            pid_3 = 21 
+            pid_4 = 21
+            
+        elif process[1] == 2:
+            pid_1 = 21
+            pid_2 = 21
+            pid_3 = 1
+            pid_4 = -1
+            
+    elif process[0] == 5:
+        pid_1 = 21
+        pid_2 = 21
+        pid_3 = 21 
+        pid_4 = 21
+        
+    else:
+        pid_1 = 0
+        pid_2 = 0
+        pid_3 = 0
+        pid_4 = 0
     
     try:
         Generator.InitializeTheRun(len(sys.argv),sys.argv)
         Process=Sherpa.MEProcess(Generator)
 
         # Incoming flavors must be added first!
-        Process.AddInFlav(1);
-        Process.AddInFlav(-1);
-        Process.AddOutFlav(1);
-        Process.AddOutFlav(-1);
+        Process.AddInFlav(pid_1);
+        Process.AddInFlav(pid_2);
+        Process.AddOutFlav(pid_3);
+        Process.AddOutFlav(pid_4);
         Process.AddOutFlav(21);
         Process.Initialize();
-
-        # First argument corresponds to particle index:
-        # index 0 correspons to particle added first, index 1 is the particle added second, and so on...
-        #Process.SetMomentum(0, 50.,0.,0.,50.)
-        #Process.SetMomentum(1, 50.,0.,0.,-50.)
-        #Process.SetMomentum(2, 50.,0.,50.,0.)
-        #Process.SetMomentum(3, 50.,0.,-50.,0.)
-        #print '\nSquared ME: ', Process.CSMatrixElement()
 
         # Momentum setting via list of floats
         Process.SetMomenta([p_1,
@@ -39,12 +94,6 @@ def calcMatEl (p_1, p_2, p_3, p_4, p_s):
                             p_s])
         MatEl = Process.CSMatrixElement()
         print '\nSquared ME: ', MatEl
-        
-        # Random momenta
-        #E_cms = 500.0
-        #tp = Process.TestPoint(E_cms)
-        #print '\nRandom test point: ', tp[0], tp[1], tp[2], tp[3]
-        #print 'Squared ME: ', Process.CSMatrixElement(), '\n'
 
     except Sherpa.Exception as exc:
         print exc
