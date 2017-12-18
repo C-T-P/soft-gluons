b0c8f0d7125088ecc385a763a782b8c5ae2cd7ed
Christian Preuss <CPreuss1@gmx.de>
2017-12-18 16:07:48 +0100

    Correct major typos

--- eikonal.py
+++ eikonal.py
@@ -1,87 +1,10 @@
 import math as m
-import cmath as cm
 import numpy as np
 import matplotlib.pyplot as plt
 import interface as sh
 from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
-
-def writeRunCard(process, E):
-    if process[0] == 1:
-        if process[1] == 1:
-            pid_1 = 1
-            pid_2 = -1
-            pid_3 = 1 
-            pid_4 = -1
-            
-        elif process[1] == 2:
-            pid_1 = 1
-            pid_2 = -1
-            pid_3 = 2
-            pid_4 = -2
-            
-        elif process[1] == 3:
-            pid_1 = 2
-            pid_2 = -1
-            pid_3 =  2
-            pid_4 = -1
-        title = "$q\\bar{q} \\to q\\bar{q}(g)$"
-            
-    elif process[0] == 2:
-        if process[1] == 1:
-            pid_1 = 1
-            pid_2 = 1
-            pid_3 = 1
-            pid_4 = 1
-            
-        elif process[1] == 2:
-            pid_1 = 1
-            pid_2 = 2
-            pid_3 = 1
-            pid_4 = 2
-        title = "$qq \\to qq(g)$"
-        
-    elif process[0] == 3:
-        pid_1 = 1
-        pid_2 = 21
-        pid_3 = 1
-        pid_4 = 21
-        
-        title = "$qg \\to qg(g)$"
-            
-    elif process[0] == 4:
-        if process[1] == 1:
-            pid_1 = 1
-            pid_2 = -1
-            pid_3 = 21 
-            pid_4 = 21
-            title = "$q\\bar{q} \\to gg(g)$"
-            
-        elif process[1] == 2:
-            pid_1 = 21
-            pid_2 = 21
-            pid_3 = 1
-            pid_4 = -1
-            title = "$gg \\to q\\bar{q}(g)$"
-            
-    elif process[0] == 5:
-        pid_1 = 21
-        pid_2 = 21
-        pid_3 = 21 
-        pid_4 = 21
-        title = "$gg \\to gg(g)$"
-        
-    else:
-        title = "not defined"   
-    
-    with open('Run.dat', 'w') as f:
-        f.write("(run){\n\tEVENTS %d\n\tSHERPA_LDADD SherpaMain;\n\tSCALES VAR{sqr(91.18)}\n}(run)" % (0))
-        f.write("\n\n(beam){\n\tBEAM_1  %d; BEAM_ENERGY_1  %.1f;\n\tBEAM_2  %d; BEAM_ENERGY_2  %.1f;\n}(beam)" % (pid_1, E, pid_2, E))
-        f.write("\n\n(isr){\n\tPDF_LIBRARY None;\n}(isr)")
-        f.write("\n\n(processes){\n\tProcess %d %d -> %d %d %d;\n\tME_Generator Amegic;\n\tOrder (3,0);\n\tEnd process;\n}(processes)" % (pid_1, pid_2, pid_3, pid_4, 21))
-        
-    return title;
 
 def minkprod(a, b):
     metric = [[1., 0., 0., 0.],[0.,-1., 0., 0.],[0., 0., -1., 0.], [0., 0., 0., -1.]]
@@ -137,17 +60,19 @@ def calcTrace (process, p_1, p_2, p_3, p_4, p_s):
     elif process[0] == 2:
         # qq -> qq
         if process[1] == 1:
+            prfct = 0.5 # due to identical particles in final state
             chi_1 = (pow(t,2)+pow(s,2))/pow(u,2)
             chi_2 = N_C*pow(s,2)/(t*u)-chi_1
             chi_3 = (pow(s,2)+pow(u,2))/pow(t,2)+1/pow(N_C,2)*(pow(s,2)+pow(t,2))/pow(u,2)-2/N_C*pow(s,2)/(t*u)
         
         # qq' -> qq'
         elif process[1] == 2:
+            prfct = 1.0
             chi_1 = 0.
             chi_2 = 0.
             chi_3 = (pow(s,2)+pow(u,2))/pow(t,2)
             
-        H = np.array([[2.*pow(C_F,2)/pow(N_C, 4)*chi_1, 2.*C_F/pow(N_C, 4)*chi_2],
+        H = prfct*np.array([[2.*pow(C_F,2)/pow(N_C, 4)*chi_1, 2.*C_F/pow(N_C, 4)*chi_2],
                     [2.*C_F/pow(N_C, 4)*chi_2, 2./pow(N_C, 2)*chi_3]])
         M = np.array([[pow(N_C,2), 0.],
                     [0., 1./4.*(pow(N_C,2)-1)]])
@@ -166,13 +91,12 @@ def calcTrace (process, p_1, p_2, p_3, p_4, p_s):
         chi_2 = (1+2*u/t)*chi_1
         chi_3 = (1-4*s*u/pow(t,2))*chi_1
                     
-        prfct = 1./(2.*N_C*(pow(N_C,2)-1))
-        H = np.array([[prfct/pow(N_C,2)*chi_1, prfct/N_C*chi_1, prfct/N_C*chi_2],
-                [prfct/N_C*chi_1, prfct*chi_1, prfct*chi_2],
-                [prfct/N_C*chi_2, prfct*chi_2, prfct*chi_3]])
-        M = np.array([[C_F*2*pow(N_C,2),0.,0.],
-                [0., C_F*(pow(N_C,2)-4),0.],
-                [0.,0.,C_F*pow(N_C,2)]])
+        H = 1./(2.*N_C*(pow(N_C,2)-1))*np.array([[1./pow(N_C,2)*chi_1, 1./N_C*chi_1, 1./N_C*chi_2],
+                [1./N_C*chi_1, chi_1, chi_2],
+                [1./N_C*chi_2, chi_2, chi_3]])
+        M = C_F*np.array([[2*pow(N_C,2),0.,0.],
+                [0., (pow(N_C,2)-4),0.],
+                [0.,0.,pow(N_C,2)]])
         
         C_12 = np.array([[0., 0., -1./2.],[0., N_C/4., -N_C/4.],[-1., -(pow(N_C,2)-4)/(4.*N_C), N_C/4.]])
         C_13 = np.array([[(pow(N_C, 2)-1)/(2.*N_C), 0., 0.],[0., -1./(2.*N_C), 0.],[0., 0., -1./(2.*N_C)]])
@@ -186,7 +110,7 @@ def calcTrace (process, p_1, p_2, p_3, p_4, p_s):
     elif process[0] == 4:
         #qqbar -> gg
         if process[1] == 1:
-            prfct = pow(N_C,2)
+            prfct = 1./(4.*pow(N_C,2)) # extra factor of 0.5 for identical final state ptcls
             
             C_12 = np.array([[(pow(N_C,2)-1)/(2.*N_C), 0., 0.],[0., -1./(2.*N_C), 0.], [0., 0., -1./(2.*N_C)]])
             C_13 = np.array([[0., 0., 1./2.],[0., -N_C/4., N_C/4.],[1., (pow(N_C,2)-4)/(4.*N_C), -N_C/4]])
@@ -197,7 +121,7 @@ def calcTrace (process, p_1, p_2, p_3, p_4, p_s):
         
         # gg -> qqbar
         elif process[1] == 2:
-            prfct = pow(pow(N_C,2)-1,2)
+            prfct = 1./(2.*pow(pow(N_C,2)-1,2))
             
             C_12 = np.array([[N_C, 0., 0.,],[0., N_C/2., 0.],[0., 0., N_C/2.]])
             C_34 = np.array([[(pow(N_C,2)-1)/(2.*N_C), 0., 0.],[0., -1./(2.*N_C), 0.], [0., 0., -1./(2.*N_C)]])
@@ -210,23 +134,23 @@ def calcTrace (process, p_1, p_2, p_3, p_4, p_s):
         chi_2 = (1+2*u/s)*chi_1
         chi_3 = (1-4*t*u/pow(s,2))*chi_1
                     
-        H = np.array([[prfct/pow(N_C,2)*chi_1, prfct/N_C*chi_1, prfct/N_C*chi_2],
-                [prfct/N_C*chi_1, prfct*chi_1, prfct*chi_2],
-                [prfct/N_C*chi_2, prfct*chi_2, prfct*chi_3]])
-        M = np.array([[C_F*2*pow(N_C,2),0.,0.],
-                [0., C_F*(pow(N_C,2)-4),0.],
-                [0.,0.,C_F*pow(N_C,2)]])
+        H = prfct*np.array([[1./pow(N_C,2)*chi_1, 1./N_C*chi_1, 1./N_C*chi_2],
+                [1./N_C*chi_1, chi_1, chi_2],
+                [1./N_C*chi_2, chi_2, chi_3]])
+        M = C_F*np.array([[2*pow(N_C,2),0.,0.],
+                [0., pow(N_C,2)-4,0.],
+                [0.,0.,pow(N_C,2)]])
         
         
         Gamma = np.zeros((3,3))
         
     # gg -> gg
     elif process[0] == 5:
-        chi_1 = 1-t*u/pow(s,2)-s*t/pow(u,2)+pow(t,2)/(s*u)
+        chi_1 = 1.-t*u/pow(s,2)-s*t/pow(u,2)+pow(t,2)/(s*u)
         chi_2 = s*t/pow(u,2)-t*u/pow(s,2)+pow(u,2)/(s*t)-pow(s,2)/(t*u)
-        chi_3 = 27/4 - 9*(s*u/pow(t,2) +1/4*t*u/pow(s,2)+1/4*s*t/pow(u,2)) + 9/2*(pow(u,2)/(s*t)+pow(s,2)/(t*u)-1/2*pow(t,2)/(s*u))
+        chi_3 = 27./4 - 9.*(s*u/pow(t,2) +1./4.*t*u/pow(s,2)+1./4.*s*t/pow(u,2)) + 9./2.*(pow(u,2)/(s*t)+pow(s,2)/(t*u)-1./2.*pow(t,2)/(s*u))
         
-        H = np.array([[9.*chi_1, 9./2.*chi_1, 9./2.*chi_2, 0., -3.*chi_1],
+        H = 0.5*1/16.*np.array([[9.*chi_1, 9./2.*chi_1, 9./2.*chi_2, 0., -3.*chi_1],
             [9./2.*chi_1, 9./4.*chi_1, 9./4.*chi_2, 0., -3./2.*chi_1],
             [9./2.*chi_2, 9./4.*chi_2, chi_3, 0., -3./2.*chi_2],
             [0., 0., 0., 0., 0.],
@@ -285,6 +209,7 @@ def calcTrace (process, p_1, p_2, p_3, p_4, p_s):
             Gamma = np.add(Gamma, minkprod(hard_p[i], hard_p[j])/(minkprod(hard_p[i], p_s)*minkprod(hard_p[j], p_s))*C)
     
     trace = np.trace(np.matmul(np.matmul(H, M), Gamma))
+    #trace = np.trace(np.matmul(H, M))
     
     return trace;
 
@@ -298,22 +223,20 @@ phi_s = m.pi/7.
 # Process specification
 process = [5,1]
 E = 7.e3
-K = np.array([0., 1., 2., 3., 4., 5., 6., 7., 8., 9., 10.])
-lambda_s = np.array([1.e-6, 5.e-6, 1.e-5, 5.e-5, 1.e-4, 5.e-4, 1.e-3, 5e-3, 1.e-2, 5.e-2, 1.e-1,]) #np.array([1e-6, 1e-1])
+K = np.array([0., 1., 2., 3., 4., 5., 6., 7., 8., 9.])
+lambda_s = np.logspace(-3, -1, 10)
 R_s = np.zeros((K.size,lambda_s.size))
 
-# calculate Matrix Elements
-MatEl = sh.calcMatEl(process, K, lambda_s, E)
-#print MatEl
+# calculate Matrix Elements and get title for plot in first variable
+plot_title, MatEl = sh.calcMatEl(process, K, lambda_s, E)
 
 # Plot setup
-plot_title = writeRunCard(process, E)
 plt.title(plot_title + '$\mathrm{~at~} E = %.1f \mathrm{~GeV}$' %(E))
 plt.xlabel('$\lambda_s$')
 plt.ylabel('$R_s$')
 plt.xscale('log') 
 
-for i in range(0, K.size):
+for i in range(1, K.size):
     N = K[i]
     for j in range(0, lambda_s.size):
         k_s = 2.*E*lambda_s[j]
@@ -323,9 +246,13 @@ for i in range(0, K.size):
         p_4 = [E,E*m.cos(phi_44+N*phi_H),E*m.sin(phi_44+N*phi_H),0]
         p_s = [k_s,k_s*m.cos(phi_s),k_s*m.sin(phi_s),0.]
         
+        #Ntrace = calcTrace(process, p_1, p_2, p_3, p_4, p_s)
+        #R_s[i,j] = Ntrace/MatEl[i,j]*(4*m.pi*alpha_s)**2
         trace = calcTrace(process, p_1, p_2, p_3, p_4, p_s)
-        R_s[i,j] = alpha_s/m.pi*trace/MatEl[i,j]
-         
+        R_s[i,j] = trace/MatEl[i,j]*(4*m.pi*alpha_s)**3
+        
+    print R_s[i]
+    
     plt.plot(lambda_s, R_s[i], label="N = %i" %(N))    
 
 plt.legend(bbox_to_anchor=(1.01, 1), loc='upper left')   
